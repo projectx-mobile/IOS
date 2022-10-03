bc40ef59a3d9e51416492f87a83df5e6283f26ec
AliakseiSipa <alexeisipa@gmail.com>
2022-10-03 21:36:30 +0300

    images corrected, tabBar items position corrected

--- Project X/Design System/TabBar VC/MainTabbarController.swift
+++ Project X/Design System/TabBar VC/MainTabbarController.swift
@@ -14,6 +14,7 @@ class MainTabBarController: UITabBarController {
      
        generateTabBar()
         setupTabBar()
+        
     }
     
     
@@ -21,7 +22,7 @@ class MainTabBarController: UITabBarController {
         viewControllers = [
             generateVC(viewController: TasksViewController(), image: UIImage(named: "home.notselected"), selectedImage: UIImage(named: "home.selected")),
             generateVC(viewController: WalletViewController(), image: UIImage(named: "wallet.notselected"), selectedImage: UIImage(named: "wallet.selected")),
-            generateVC(viewController: AddTaskViewController(), image: UIImage(named: "plusButton"), selectedImage: UIImage(named: "plusButton")),
+            generatePlusVC(viewController: AddTaskViewController(), image: UIImage(named: "plusButton"), selectedImage: UIImage(named: "plusButton")),
             generateVC(viewController: RewardsViewController(), image: UIImage(named: "rewards.notselected"), selectedImage: UIImage(named: "rewards.selected")),
             generateVC(viewController: ProfileViewController(), image: UIImage(named: "profile.notselected"), selectedImage: UIImage(named: "profile.selected"))
         ]
@@ -29,8 +30,18 @@ class MainTabBarController: UITabBarController {
     private func generateVC(viewController: UIViewController, image: UIImage?, selectedImage: UIImage?)-> UIViewController {
         viewController.tabBarItem.image = image
         viewController.tabBarItem.selectedImage = selectedImage
+        viewController.tabBarItem.imageInsets = UIEdgeInsets(top: 5, left: 0, bottom: -10, right: 0)
         return viewController
     }
+    
+    private func generatePlusVC(viewController: UIViewController, image: UIImage?, selectedImage: UIImage?)-> UIViewController {
+        viewController.tabBarItem.image = image
+        viewController.tabBarItem.selectedImage = selectedImage
+        viewController.tabBarItem.imageInsets = UIEdgeInsets(top: -5, left: 0, bottom: 0, right: 0)
+        return viewController
+    }
+    
+    
     private func setupTabBar() {
         let roundLayer = CAShapeLayer()
         let biezerPath = UIBezierPath(roundedRect: CGRect(x: 0, y: 0, width: view.frame.width, height: 86),
@@ -38,7 +49,6 @@ class MainTabBarController: UITabBarController {
         roundLayer.path = biezerPath.cgPath
         tabBar.layer.insertSublayer(roundLayer, at: 0)
         tabBar.itemWidth = 48
-        tabBar.itemPositioning = .centered
         roundLayer.fillColor = UIColor(red: 0.094, green: 0.098, blue: 0.145, alpha: 1).cgColor
         tabBar.tintColor = .white
     }
