7bf4407fcd19715cc140f7fc3128adcb1f63d414
Nataliya Lazouskaya <93728977+NataliyaLaz@users.noreply.github.com>
2022-10-31 17:30:37 +0300

    add label change and tasks filter

--- Project X/ChildHomeScreenModule/ChildHomeScreenInteractor/ChildHomeScreenInteractor.swift
+++ Project X/ChildHomeScreenModule/ChildHomeScreenInteractor/ChildHomeScreenInteractor.swift
@@ -8,7 +8,6 @@
 import Foundation
 
 final class ChildHomeScreenInteractor: ChildHomeScreenInteractorInputProtocol {
-
     unowned let presenter: ChildHomeScreenInteractorOutputProtocol
 
     required init(presenter: ChildHomeScreenInteractorOutputProtocol) {
@@ -20,12 +19,25 @@ final class ChildHomeScreenInteractor: ChildHomeScreenInteractorInputProtocol {
         KidsNotification(text: "У тебя появилась новая задача на сегодня, постарайся успеть."),
         KidsNotification(text: "Исткело время, выделенное на выполнение задачи.")
     ]
+    
+    var activeTasksFromEntity: [KidsTask] = [
+        KidsTask(title: "Clean the room", description: .today, done: false, points: 30),
+        KidsTask(title: "Water the flowers", description: .today, done: false, points: 10),
+        KidsTask(title: "Wash the cat", description: .today, done: false, points: 40),
+        KidsTask(title: "Do your homework", description: .missed, done: false, points: 20),
+        KidsTask(title: "Read 30 pages", description: .today, done: false, points: 30),
+    ]
 
     func provideNumberOfNotifications() {
         let numberOfNotifications = kidsNotificationsFromEntity.count
         presenter.receiveNumberOfNotifications(number: numberOfNotifications)
     }
-
+    
+    func provideNumberOfActiveTasks() {
+        let numberOfActiveTasks = activeTasksFromEntity.filter { $0.description == .today && $0.done == false}.count
+        presenter.receiveNumberOfActiveTasks(number: numberOfActiveTasks)
+    }
+    
     func provideTextOfNotification() {
         let text = kidsNotificationsFromEntity[0].text
         presenter.receiveTextOfNotification(text: text)
--- Project X/ChildHomeScreenModule/ChildHomeScreenInteractor/ChildHomeScreenInteractorIO.swift
+++ Project X/ChildHomeScreenModule/ChildHomeScreenInteractor/ChildHomeScreenInteractorIO.swift
@@ -10,11 +10,13 @@ import Foundation
 protocol ChildHomeScreenInteractorInputProtocol: AnyObject {
     init(presenter: ChildHomeScreenInteractorOutputProtocol)
     func provideNumberOfNotifications()
+    func provideNumberOfActiveTasks()
     func provideTextOfNotification()
     func deleteFirstNotification()
 }
 
 protocol ChildHomeScreenInteractorOutputProtocol: AnyObject {
     func receiveNumberOfNotifications(number: Int)
+    func receiveNumberOfActiveTasks(number: Int)
     func receiveTextOfNotification(text: String)
 }
