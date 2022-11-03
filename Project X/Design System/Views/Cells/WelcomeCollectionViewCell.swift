8f78580dfe2303c8f4aee5837a6267d4dccdba0b
Alexandra Zakharevich <alexa.zakharevich_iosdev@mail.ru>
2022-11-03 19:07:36 +0300

    add new branch with animations

---
+++ Project X/Design System/Views/Cells/WelcomeCollectionViewCell.swift
@@ -0,0 +1,76 @@
+//
+//  WelcomeCollectionViewCell.swift
+//  Project X
+//
+//  Created by Александра Захаревич on 3.11.22.
+//
+
+import UIKit
+import Lottie
+
+ final class WelcomeCollectionViewCell: UICollectionViewCell {
+    
+    static let reuseID = "WelcomeCollectionViewCell"
+    
+     let welcomeSubtitleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.font = .nexaDemoBold32()
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
+        var paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineHeightMultiple = 0.74
+        label.attributedText = NSMutableAttributedString(string: "", attributes: [NSAttributedString.Key.paragraphStyle: paragraphStyle])
+        label.textAlignment = .left
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+     var animationView = AnimationView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+        setupAnimation()
+        
+        welcomeSubtitleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 0).isActive = true
+        welcomeSubtitleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8.5).isActive = true
+        welcomeSubtitleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -8.5).isActive = true
+        welcomeSubtitleLabel.widthAnchor.constraint(equalToConstant: 343).isActive = true
+        
+        animationView.topAnchor.constraint(equalTo: welcomeSubtitleLabel.bottomAnchor, constant: 32).isActive = true
+        animationView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 0).isActive = true
+        animationView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 0).isActive = true
+        animationView.heightAnchor.constraint(equalToConstant: 325).isActive = true
+        
+        welcomeSubtitleLabel.backgroundColor = .white
+        animationView.backgroundColor = .yellow
+        backgroundColor = .blue
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension WelcomeCollectionViewCell {
+    func setup(_ model: WelcomeCellModel) {
+        welcomeSubtitleLabel.text = model.subtitle
+        animationView.animation = .named(model.animationName)
+        animationView.play()
+    }
+}
+
+private extension WelcomeCollectionViewCell {
+    func setupViews() {
+        addSubview(welcomeSubtitleLabel)
+    }
+
+    func setupAnimation(){
+        animationView.backgroundColor = .primaryPureWhite
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = .loop
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(animationView)
+    }
+ }
---
+++ Project X/Design System/Views/View/WelcomeCollectionView.swift
@@ -0,0 +1,51 @@
+//
+//  WelcomeCollectionView.swift
+//  Project X
+//
+//  Created by Александра Захаревич on 3.11.22.
+//
+
+import UIKit
+import Lottie
+
+class WelcomeCollectionView: UICollectionView, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    var cells = [WelcomeCellModel]()
+    
+    init() {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        super.init(frame: .zero, collectionViewLayout: layout)
+        
+        delegate = self
+        dataSource = self
+        register(WelcomeCollectionViewCell.self, forCellWithReuseIdentifier: WelcomeCollectionViewCell.reuseID)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    func setCells(cells: [WelcomeCellModel]) {
+        self.cells = cells
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return cells.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: WelcomeCollectionViewCell.reuseID, for: indexPath) as! WelcomeCollectionViewCell
+        cell.setup(cells[indexPath.row])
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: frame.width, height: 429)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
